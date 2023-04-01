6af66b3b82fa163692e58187a82675f3c2e2d42f
Gabriel Pereira de Carvalho <nazter57@usp.br>
2023-04-01 11:48:44 +0200

    :memo: problem 5 refactor

--- 5_LongestPalindromicSubstring.py
+++ 5_LongestPalindromicSubstring.py
@@ -1,32 +1,34 @@
+
 class Solution:
+
+    def extendPalindrome(self, s: str, leftPointer: int, rightPointer: int, oddPalindrome: bool) -> tuple:
+        extensionCount = 1 if oddPalindrome else 2
+        while(leftPointer >= 0 and rightPointer < len(s) and s[leftPointer]==s[rightPointer]):
+                #we check to see if borders of palindrome can be extended
+                leftPointer -= 1
+                rightPointer += 1
+                extensionCount += 2
+        #return (begin, length) of palindrome found
+        return (leftPointer + 1, extensionCount)
+
     def longestPalindrome(self, s: str) -> str:
         longestPalindromeBegin = 0
         longestPalindromeLength = 1
+
         for i in range(0, len(s)):
-            #compute largest palindrome centered at i OR pair(i, i+1)
-            leftPointer = i-1
-            rightPointer = i+1
-            palindromeLength = 1
-            while(leftPointer >= 0 and rightPointer < len(s) and s[leftPointer]==s[rightPointer]):
-                leftPointer-=1
-                rightPointer+=1
-                palindromeLength+=2
-            if(palindromeLength > longestPalindromeLength):
-                longestPalindromeBegin = leftPointer + 1
-                longestPalindromeLength = palindromeLength
+            #compute largest palindrome centered at i (odd length)
+            (largestOddBegin, largestOddLength) = self.extendPalindrome(s, i-1, i+1, True)
+            if(largestOddLength > longestPalindromeLength):
+                longestPalindromeBegin = largestOddBegin
+                longestPalindromeLength = largestOddLength
+            #compute largest palindrome centered at pair i, i+1 (even length)
             if(i>=1 and s[i]==s[i-1]):
-                leftPointer2 = i-2
-                rightPointer2 = i+1
-                palindromeLength2 = 2
-                while(leftPointer2 >= 0 and rightPointer2 < len(s) and s[leftPointer2]==s[rightPointer2]):
-                    leftPointer2-=1
-                    rightPointer2+=1
-                    palindromeLength2+=2
-                if(palindromeLength2 > longestPalindromeLength):
-                    longestPalindromeBegin = leftPointer2 + 1
-                    longestPalindromeLength = palindromeLength2
+                (largestEvenBegin, largestEvenLength) = self.extendPalindrome(s, i-2, i+1, False)
+                if(largestEvenLength > longestPalindromeLength):
+                    longestPalindromeBegin = largestEvenBegin
+                    longestPalindromeLength = largestEvenLength
+        #now we build answer to return
         answer = ""
         for i in range(longestPalindromeBegin, longestPalindromeBegin+longestPalindromeLength):
             answer += s[i]
         return answer
-    
